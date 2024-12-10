ddb6062a54173339b04cb521219372841917a6e7
Abdullah  Demir <Abdullah.demr@hotmail.com>
2024-12-10 14:17:59 +0300

    yapay zeka

---
+++ YapayZeka/Controllers/FAQsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace YapayZeka.Controllers
+{
+    public class FAQsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ YapayZeka/YapayZeka/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YapayZeka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
